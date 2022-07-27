b4d22cbd996f4d4dd79d67b5db423bc227b8bb3c
sodaop <saiphirun_p@cmu.ac.th>
2022-07-27 14:40:38 +0700

    1.11,2.9

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
+import EventDetailView from '../views/EventDetailView.vue'
 const routes = [
   {
     path: '/',
     name: 'EventList',
-    component: EventListView
+    component: EventListView,
+    props: (route) => ({ page: parseInt(route.query.page) || 1, limit: 1 })
   },
   {
     path: '/about',
     name: 'about',
     component: AboutView
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetailView',
+    component: EventDetailView,
+    props: true
   }
 ]
 
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -6,6 +6,25 @@
       :key="event.id"
       :event="event"
     ></EventCard>
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+      >
+        Prev Page
+      </router-link>
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+      >
+        Next Page
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -13,24 +32,45 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from '@vue/runtime-core'
 export default {
   name: 'EventListView',
+  props: {
+    page: {
+      type: Number,
+      required: true
+    },
+    limit: {
+      type: Number,
+      required: true
+    }
+  },
   components: {
     EventCard
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0
     }
   },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    watchEffect(() => {
+      EventService.getEvents(this.limit, this.page)
+        .then((response) => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count']
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      let totalPages = Math.ceil(this.totalEvents / 2)
+      return this.page < totalPages
+    }
   }
 }
 </script>
@@ -39,5 +79,20 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
